66de9018de20b50c4fd6dd8ab601293510bd5a4a
Nelson Caudill <nelson.a.caudill@gmail.com>
2022-02-03 20:31:02 -0500

    style NavLinks with remix api

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,15 +1,30 @@
-import { Link } from "remix";
+import { NavLink } from "remix";
+
 import styled from "styled-components";
 
-const links = ["All", "Gin", "Agave", "Whiskey", "Rum", "Other"];
+const links = [
+  { href: "/", text: "All" },
+  { href: "/gin", text: "Gin" },
+  { href: "/agave", text: "Agave" },
+  { href: "/whiskey", text: "Whiskey" },
+  { href: "/rum", text: "Rum" },
+  { href: "/other", text: "Other" },
+];
 
 const Nav = () => {
+  const styles = ({ isActive }: any) => ({
+    color: isActive ? "red" : "inherit",
+    textDecoration: "none",
+    paddingLeft: "12px",
+    paddingRight: "12px",
+  });
+
   return (
     <Wrapper>
-      {links.map((link) => (
-        <StyledLink to={`/drinks/${link.toLocaleLowerCase()}`}>
-          {link}
-        </StyledLink>
+      {links.map(({ href, text }) => (
+        <NavLink key={href} to={href} style={styles}>
+          {text}
+        </NavLink>
       ))}
     </Wrapper>
   );
@@ -17,13 +32,6 @@ const Nav = () => {
 
 const Wrapper = styled.nav`
   display: flex;
-`;
-
-const StyledLink = styled(Link)`
-  flex: 1;
-  padding: 6px;
-  text-decoration: none;
-  text-align: center;
 `;
 
 export default Nav;
