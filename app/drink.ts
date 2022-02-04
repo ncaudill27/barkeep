55a1c5c8b957d2166e517ebd42ba884e57a9c8f9
Nelson Caudill <nelson.a.caudill@gmail.com>
2022-02-04 00:18:07 -0500

    parse param helper functions and commenting in drinks

--- app/drink.ts
+++ app/drink.ts
@@ -1,6 +1,16 @@
 import client from "./sanity.server";
 import invariant from "tiny-invariant";
 
+// helper function used in getDrinksByCategory
+function parseCategoryParam(param: string): string {
+  if (param === "agave")
+    return '*[count((categories[]->title)[@ in ["Tequila", "Agave"]]) > 0]';
+  else if (param == "other")
+    return '*[count((categories[]->title)[@ in ["Vodka"]]) > 0]';
+  else
+    return "*[count((categories[]->title)[lower(@) == $category]) > 0]{name}";
+}
+
 export type Drink = {
   name: string;
   active: boolean;
@@ -9,25 +19,29 @@ export type Drink = {
   ingredients: Array<{ name: string; amount: string; measurement: string }>;
 };
 
+// abstracted fetch call to SanityClient
 async function sanityFetchDrinks(query: string, params = {}): Promise<Drink[]> {
   return client.fetch(query, params).then((drinks: Drink[]) => {
     return drinks;
   });
 }
 
+// drink index
 export async function getDrinks(): Promise<Drink[]> {
   const query = '*[_type == "dinnerCocktail"] {name}';
 
   return await sanityFetchDrinks(query);
 }
 
+// $category index
 export async function getDrinksByCategory(category: string): Promise<Drink[]> {
-  const query =
-    "*[count((categories[]->title)[lower(@) == $category]) > 0]{name}";
+  const query = parseCategoryParam(category);
+
   const params = { category };
   return await sanityFetchDrinks(query, params);
 }
 
+// $drink get
 export async function getDrink(name: string): Promise<Drink> {
   const query = `*[_type == "dinnerCocktail" && name == $name] {name, active, garnish, glassware, ingredients }`;
   const params = { name };
