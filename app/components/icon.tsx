4776d235c589c6be2056b2c845dbce9385232bf1
Nelson Caudill <nelson.a.caudill@gmail.com>
2022-02-10 02:01:13 -0500

    add styling to ICON object

--- app/components/icon.tsx
+++ app/components/icon.tsx
@@ -5,12 +5,14 @@ type IconProps = {
   icon: "martini";
 };
 
-const ATTRIBUTION = {
+const ICON = {
   martini: {
     author: "Juraj Sedlák",
     profile: "https://thenounproject.com/yumminky/",
     twitter: "https://twitter.com/yumminky",
     iconLink: "https://thenounproject.com/icon/martini-glass-876587/",
+    aspectRatio: 1.9178,
+    width: "200px",
   },
 };
 
@@ -26,28 +28,34 @@ export default function Icon({ icon }: IconProps) {
       throw new Error("Icon Error: Invalid icon prop supplied");
   }
 
-  const StyledTag = styled(Tag)`
-    height: 100%;
-    width: 100%;
-  `;
-
   return (
     <Wrapper
-      data-iconauthor={ATTRIBUTION[icon].author}
-      data-iconauthorprofile={ATTRIBUTION[icon].profile}
-      data-iconauthortwitter={ATTRIBUTION[icon].twitter}
-      data-iconlocation={ATTRIBUTION[icon].iconLink}
+      data-iconauthor={ICON[icon].author}
+      data-iconauthorprofile={ICON[icon].profile}
+      data-iconauthortwitter={ICON[icon].twitter}
+      data-iconlocation={ICON[icon].iconLink}
+      style={{
+        "--aspect-ratio": ICON[icon].aspectRatio,
+        "--width": ICON[icon].width,
+      }}
     >
-      <StyledTag />
+      <Tag />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+type IconWrapperProps = {
+  style: {
+    "--aspect-ratio": number;
+    "--width": string;
+  };
+};
+
+const Wrapper = styled.div<IconWrapperProps>`
   position: fixed;
   top: 4rem;
   right: -16px;
 
-  width: 200px;
-  height: calc(200px * 1.9178);
+  width: var(--width);
+  height: calc(var(--width) * var(--aspect-ratio));
 `;
