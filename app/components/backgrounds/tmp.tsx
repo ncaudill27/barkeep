11b5fb565da26b4b094965559e5069ff2ae408c0
Nelson Caudill <nelson.a.caudill@gmail.com>
2022-06-07 15:27:02 -0400

    delete all old background files

--- app/components/backgrounds/tmp.tsx
+++
@@ -1,5 +0,0 @@
-function TMP() {
-  return (
-    <svg id="visual" viewBox="0 0 540 960" width="540" height="960" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><g transform="translate(0, 960)"><path d="M0 -432C87.8 -427.8 175.7 -423.6 216 -374.1C256.3 -324.6 249.1 -229.9 277.1 -160C305.2 -90.1 368.6 -45.1 432 0L0 0Z" fill="#f7e3d8"></path><path d="M0 -288C58.6 -285.2 117.1 -282.4 144 -249.4C170.9 -216.4 166 -153.3 184.8 -106.7C203.5 -60.1 245.7 -30 288 0L0 0Z" fill="#f7cdca"></path><path d="M0 -144C29.3 -142.6 58.6 -141.2 72 -124.7C85.4 -108.2 83 -76.6 92.4 -53.3C101.7 -30 122.9 -15 144 0L0 0Z" fill="#f5c2c8"></path></g></svg>
-  );
-}
